3b116192d7470c8173c8a30b38a805c76fa3024b
jesus <guerrero.tlahcuilo@gmail.com>
2016-03-31 12:06:45 -0600

    Mi primer repo

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
+#Registra nuestro objeto Post ene el administrador
+#de Django para poder consualtar  o actualizar la la informaciòn del o de los Post
+
 admin.site.register(Post)
